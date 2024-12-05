35043a414807683b2e95a5dec29621210bc40d8d
Chikondi Kamwendo <kondie208@gmail.com>
2024-12-05 09:25:24 +0200

    Implemented connectivity service contract.

--- lib/core/services/app_connectivity_service.dart
+++ lib/core/services/app_connectivity_service.dart
@@ -1,9 +1,15 @@
+import 'package:observe_internet_connectivity/observe_internet_connectivity.dart';
 import '../contracts/services/connectivity_service.dart';
 
 class AppConnectivityService implements ConnectivityService {
+  late final InternetConnectivity _internetConnectivity;
+
+  AppConnectivityService() {
+    _internetConnectivity = InternetConnectivity();
+  }
+
   @override
   Future<bool> hasInternetConnection() {
-    // TODO: implement hasInternetConnection
-    throw UnimplementedError();
+    return _internetConnectivity.hasInternetConnection;
   }
 }
